f997fb1384e64ee5d50455ba5c8a29c60f9c4b13
Timur <tima@MacBook-Air-Timur.local>
2021-07-27 16:07:29 +0600

    add Class_Based_Views.

--- product/urls.py
+++ product/urls.py
@@ -1,13 +1,16 @@
 from django.contrib import admin
 from django.urls import path
+
+from .classViews import CategoryListView, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
 from .views import home_page, product_list, product_detail, create_product, update_product, delete_product
 
 urlpatterns = [
-    path('', home_page, name='home'),
-    path('<str:slug>/', product_list, name='list'),
-    path('product/<int:id>/', product_detail, name='detail'),
-    path('product/create/', create_product, name='create_product'),
-    path('product/update/<int:id>', update_product, name='update_product'),
-    path('product/delete/<int:id>', delete_product, name='delete_product'),
+    path('', CategoryListView.as_view(), name='home'),
+    path('<str:slug>/', ProductListView.as_view(), name='list'),
+    path('product/<int:id>/', ProductDetailView.as_view(), name='detail'),
+    path('product/create/', ProductCreateView.as_view(), name='create_product'),
+    path('product/update/<int:id>', ProductUpdateView.as_view(), name='update_product'),
+    path('product/delete/<int:id>', ProductDeleteView.as_view(), name='delete_product'),
 
 ]
